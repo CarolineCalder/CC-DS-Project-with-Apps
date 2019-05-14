609a206708459b964b2ce00692cf4ab52c9dd216
LindsayCalder <lindsaydesign.lc@gmail.com>
2019-05-14 15:09:41 -0600

    added cards to home

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { MDBEdgeHeader, MDBFreeBird, MDBCol, MDBRow, MDBIcon, MDBJumbotron, MDBBtn, MDBContainer, MDBCardTitle, MDBNavLink, MDBNav, MDBCardImage, MDBCardBody, MDBCardText } from "mdbreact";
+import { MDBEdgeHeader, MDBFreeBird, MDBCol, MDBRow, MDBIcon, MDBJumbotron, MDBBtn, MDBContainer, MDBCardTitle, MDBNavLink, MDBNav, MDBCardImage, MDBCardBody, MDBCardText, MDBCard, MDBCardGroup } from "mdbreact";
 import "./HomePage.css";
 
 class HomePage extends React.Component {
@@ -24,185 +24,69 @@ class HomePage extends React.Component {
           </MDBCol>
         </MDBRow>
         </MDBFreeBird>
+        
         <MDBContainer>
-          <MDBRow>
-            <MDBCol md="10" className="mx-auto mt-4">
-              <h2 className="text-center my-4 font-weight-bold">
-                Why is it so great?
-              </h2>
-              <p className="text-center">
-                Google has designed a Material Design to make the web more
-                beautiful and more user-friendly.
-              </p>
-              <p className="text-center">
-                Twitter has created a Bootstrap to support you in faster and
-                easier development of responsive and effective websites.
-              </p>
-              <p className="text-center">
-                We present you a framework containing the best features of both
-                of them - Material Design for Bootstrap.
-              </p>
-              <hr className="my-5" />
-              <MDBRow id="categories">
-                <MDBCol md="4" className="mb-5">
-                  <MDBCol size="2" md="2" className="float-left">
-                    <MDBIcon icon="css3" brand className="pink-text" size="2x" />
-                  </MDBCol>
-                  <MDBCol size="10" md="8" lg="10" className="float-right">
-                    <h4 className="font-weight-bold">CSS</h4>
-                    <p className="grey-text">
-                      Animations, colours, shadows, skins and many more! Get to
-                      know all our css styles in one place.
-                    </p>
-                    <MDBNavLink
-                      tag="button"
-                      className="btn btn-sm indigo darken-3 text-white" 
-                      to="css"
-                    >
-                      Learn more
-                    </MDBNavLink>
-                  </MDBCol>
-                </MDBCol>
-                <MDBCol md="4" className="mb-5">
-                  <MDBCol size="2" md="2" className="float-left">
-                    <MDBIcon icon="cubes" className="blue-text" size="2x" />
-                  </MDBCol>
-                  <MDBCol size="10" md="8" lg="10" className="float-right">
-                    <h4 className="font-weight-bold">COMPONENTS</h4>
-                    <p className="grey-text">
-                      Ready-to-use components that you can use in your
-                      applications. Both basic and extended versions!
-                    </p>
-                    <MDBNavLink
-                      tag="button"
-                      className="btn btn-sm indigo lighten-2 text-white"
-                      to="/components"
-                    >
-                      Learn more
-                    </MDBNavLink>
-                  </MDBCol>
-                </MDBCol>
-                <MDBCol md="4" className="mb-5">
-                  <MDBCol size="2" md="2" className="float-left">
-                    <MDBIcon icon="code" className="green-text" size="2x" />
-                  </MDBCol>
-                  <MDBCol size="10" md="8" lg="10" className="float-right">
-                    <h4 className="font-weight-bold">ADVANCED</h4>
-                    <p className="grey-text">
-                      Advanced components such as charts, carousels, tooltips
-                      and popovers. All in Material Design version.
-                    </p>
-                    <MDBNavLink
-                      tag="button"
-                      className="btn btn-sm indigo darken-3 text-white" 
-                      to="advanced"
-                    >
-                      Learn more
-                    </MDBNavLink>
-                  </MDBCol>
-                </MDBCol>
-              </MDBRow>
-              <MDBRow id="categories">
-                <MDBCol md="4" className="mb-5">
-                  <MDBCol size="2" md="2" className="float-left">
-                    <MDBIcon icon="bars" className="pink-text" size="2x" />
-                  </MDBCol>
-                  <MDBCol size="10" md="8" lg="10" className="float-right">
-                    <h4 className="font-weight-bold">NAVIGATION</h4>
-                    <p className="grey-text">
-                      Ready-to-use navigation layouts, navbars, breadcrumbs and
-                      much more! Learn more about our navigation components.
-                    </p>
-                    <MDBNavLink
-                      tag="button"
-                      className="btn btn-sm indigo darken-3 text-white"
-                      to="/navigation"
-                    >
-                      Learn more
-                    </MDBNavLink>
-                  </MDBCol>
-                </MDBCol>
-                <MDBCol md="4" className="mb-5">
-                  <MDBCol size="2" md="2" className="float-left">
-                    <MDBIcon icon="edit" className="blue-text" size="2x" />
-                  </MDBCol>
-                  <MDBCol size="10" md="8" lg="10" className="float-right">
-                    <h4 className="font-weight-bold">FORMS</h4>
-                    <p className="grey-text">
-                      Inputs, autocomplete, selecst, date and time pickers.
-                      Everything in one place is ready to use!
-                    </p>
-                    <MDBNavLink
-                      tag="button"
-                      className="btn btn-sm indigo lighten-2 text-white"
-                      to="/forms"
-                    >
-                      Learn more
-                    </MDBNavLink>
-                  </MDBCol>
-                </MDBCol>
-                <MDBCol md="4" className="mb-5">
-                  <MDBCol size="2" md="2" className="float-left">
-                    <MDBIcon icon="table" className="green-text" size="2x" />
-                  </MDBCol>
-                  <MDBCol size="10" md="8" lg="10" className="float-right">
-                    <h4 className="font-weight-bold">TABLES</h4>
-                    <p className="grey-text">
-                      Basic and advanced tables. Responsive, datatables, with
-                      sorting, searching and export to csv.
-                    </p>
-                    <MDBNavLink
-                      tag="button"
-                      className="btn btn-sm indigo darken-3 text-white" 
-                      to="tables"
-                    >
-                      Learn more
-                    </MDBNavLink>
-                  </MDBCol>
-                </MDBCol>
-              </MDBRow>
-              <MDBRow id="categories" center>
-                <MDBCol md="4" className="mb-5">
-                  <MDBCol size="2" md="2" className="float-left">
-                    <MDBIcon icon="window-restore" className="pink-text" size="2x" />
-                  </MDBCol>
-                  <MDBCol size="10" md="8" lg="10" className="float-right">
-                    <h4 className="font-weight-bold">MODALS</h4>
-                    <p className="grey-text">
-                      Modals used to display advanced messages to the user.
-                      Cookies, logging in, registration and much more.
-                    </p>
-                    <MDBNavLink
-                      tag="button"
-                      className="btn btn-sm indigo darken-3 text-white" 
-                      to="modals"
-                    >
-                      Learn more
-                    </MDBNavLink>
-                  </MDBCol>
-                </MDBCol>
-                <MDBCol md="4" className="mb-5">
-                  <MDBCol size="2" md="2" className="float-left">
-                    <MDBIcon icon="arrows-alt" className="blue-text" size="2x" />
-                  </MDBCol>
-                  <MDBCol size="10" md="8" lg="10" className="float-right">
-                    <h4 className="font-weight-bold">ADDONS</h4>
-                    <p className="grey-text">
-                      Google Maps, Social Buttons, Pre-built Contact Forms and
-                      Steppers. Find out more about our extended components.
-                    </p>
-                    <MDBNavLink
-                      tag="button"
-                      className="btn btn-sm indigo lighten-2 text-white"
-                      to="/addons"
-                    >
-                      Learn more
-                    </MDBNavLink>
-                  </MDBCol>
-                </MDBCol>
-              </MDBRow>
-            </MDBCol>
-          </MDBRow>
+        <MDBCardGroup>
+          <MDBCard>
+            <MDBCardImage
+              src="https://mdbootstrap.com/img/Photos/Others/images/49.jpg"
+              alt="MDBCard image cap"
+              top
+              hover
+              overlay="white-slight"
+            />
+            <MDBCardBody>
+              <MDBCardTitle tag="h5">Panel title</MDBCardTitle>
+              <MDBCardText>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </MDBCardText>
+              <MDBBtn color="primary" size="md">
+                read more
+              </MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
+
+          <MDBCard>
+            <MDBCardImage
+              src="https://mdbootstrap.com/img/Photos/Others/images/48.jpg"
+              alt="MDBCard image cap"
+              top
+              hover
+              overlay="white-slight"
+            />
+            <MDBCardBody>
+              <MDBCardTitle tag="h5">Panel title</MDBCardTitle>
+              <MDBCardText>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </MDBCardText>
+              <MDBBtn color="primary" size="md">
+                read more
+              </MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
+
+          <MDBCard>
+            <MDBCardImage
+              src="https://mdbootstrap.com/img/Photos/Others/images/77.jpg"
+              alt="MDBCard image cap"
+              top
+              hover
+              overlay="white-slight"
+            />
+            <MDBCardBody>
+              <MDBCardTitle tag="h5">Panel title</MDBCardTitle>
+              <MDBCardText>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </MDBCardText>
+              <MDBBtn color="primary" size="md">
+                read more
+              </MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCardGroup>
         </MDBContainer>
       </div>
     );
